ed058c833b20fa80619dc335977c6fbcca111bc8
Tejas Upadhyay <98517420+Tej1109@users.noreply.github.com>
2023-07-13 09:08:06 +0530

    Update sms.py
    removed credentials

--- sms.py
+++ sms.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 
-account_sid = 'ACdcc2236233d7893f3895195ef8748d58'
-auth_token = '195540631b942531192db4e1e731dce1'
+account_sid = 'account_sid'
+auth_token = 'auth token'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    from_='+16812485955',
+    from_='generated mobile number',
     body="Hi, do you want to eat something?",
-    to='+919267939323'
+    to='Mobile number to be sent to'
 )
 
 print(message.sid)
